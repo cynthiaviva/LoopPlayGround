f0f00c1710965e4bb63abfb4ad775fe5cd3dc235
edward <edwardmjc@gmail.com>
2018-08-31 15:56:20 +0800

    Initial Commit

---
+++ LoopPlayGround/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoopPlayGround
+//
+//  Created by edward on 2018/8/31.
+//  Copyright © 2018年 Melon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
